7fae081ca18b5329634368e20db94998ac1b0f35
AndreoAugusto <augustoandreo.ds@gmail.com>
2025-03-28 16:51:02 -0300

    4- 28.03

--- src/main/java/org/example/Entities/Contato.java
+++ src/main/java/org/example/Entities/Contato.java
@@ -18,16 +18,51 @@ public class Contato implements Serializable {
     private Fornecedor conFornecedor;
 
     @Column(length = 25, name = "NUMERO", nullable = false)
-    private String conNumero;
+    private String Numero;
 
     @Column(length = 25, name = "EMAIL", nullable = false)
-    private String conEmail;
+    private String Email;
 
 
     public Contato() {
     }
 
+    public Contato(Long id, Fornecedor conFornecedor, String numero, String email) {
+        this.id = id;
+        this.conFornecedor = conFornecedor;
+        Numero = numero;
+        Email = email;
+    }
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public Fornecedor getConFornecedor() {
+        return conFornecedor;
+    }
+
+    public void setConFornecedor(Fornecedor conFornecedor) {
+        this.conFornecedor = conFornecedor;
+    }
+
+    public String getNumero() {
+        return Numero;
+    }
+
+    public void setNumero(String numero) {
+        Numero = numero;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
 }
--- src/main/java/org/example/Services/FornecedorServices.java
+++ src/main/java/org/example/Services/FornecedorServices.java
@@ -2,15 +2,37 @@ package org.example.Services;
 
 import org.example.Entities.Contato;
 import org.example.Entities.Fornecedor;
+import org.example.Repositories.FornecedorRepositories;
 import org.example.dto.FornecedorDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class FornecedorServices {
 
+    @Autowired
+    private FornecedorRepositories repository;
 
+    public List<Fornecedor>findAll()    {
+        return repository.findAll();
+    }
 
+    public Optional<Fornecedor> findById(Long id)   {
+        return  repository.findById(id);
+    }
 
+    public Fornecedor insert(Fornecedor fornecedor) {
+        return repository.save(fornecedor);
+    }
 
-    public Fonecedor update(Long id, FornecedorDTO objDto) {
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Fornecedor update(Long id, FornecedorDTO objDto) {
         try {
             Fornecedor entity = findById(id);
 
@@ -20,9 +42,13 @@ public class FornecedorServices {
             entity.setStatus(objDto.getStatus());
 
             Contato contato = entity.getContatos().get(0);
-            contato.setConNumero(objDto.getNumero());
-            contato.setConEmail(objDto.getEmail());
+            contato.setNumero(objDto.getNumero());
+            contato.setEmail(objDto.getEmail());
 
+            return FornecedorRepositories.save(entity);
+
+        } catch (Exception e)  {
+            throw new RuntimeException("Tentativa Inutil de atualizar.");
         }
 
 
